76611c4b0f9172929111f4474200aaa9108313ef
Tommy Le <tommyle1624@gmail.com>
2023-12-11 00:21:56 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+private:
+    int balance = 0;
+    int interest = 0;
+
+public:
+    Account() {}
+
+    Account(int a) : balance(a) {
+        interest += balance / 10;
+    }
+
+    int getTotal() {
+        return balance + interest;
+    }
+
+    // Overloaded + operator
+    Account operator+(const Account& other) const {
+        Account result;
+        result.balance = balance + other.balance;
+        result.interest = interest + other.interest;
+        return result;
+    }
+};
+
+int main() {
+    int n1, n2;
+    cin >> n1 >> n2;
+    Account a(n1);
+    Account b(n2);
+    Account res = a + b;
+
+    cout << res.getTotal();
+
+    return 0;
+}
